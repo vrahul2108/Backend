bebe73004dfa8df097258eb907d587f8fc98f5ec
vrahul2108 <140827837+vrahul2108@users.noreply.github.com>
2025-12-05 23:53:51 +0530

    adding middleware

--- AuthApp/middleware/auth.js
+++ AuthApp/middleware/auth.js
@@ -1,1 +1,19 @@
 //for authentication - auth   for autherization - isStudent, isAdmin
+
+const jwt = require("jsonwebtoken");
+require('dotenv').config();
+
+exports.auth = (req, res, next) =>{
+    try{
+        const token = req.body.token;
+
+        if(!token){
+            res.status(401).json({
+                success: false,
+                message: 'Toke n Missing'
+            })
+        }
+    }catch(e){
+
+    }
+}
